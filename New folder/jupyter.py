de9d4420576718ab4c424ef17f8dbb06a5669b2d
Ravindi Wickramasinghe <121094369+Ravindi2022@users.noreply.github.com>
2025-03-10 22:39:21 +0530

    Update jupyter.py

--- New folder/jupyter.py
+++ New folder/jupyter.py
@@ -19,3 +19,36 @@ def move(state,action):
     new_state = (state[0] + moves[action][0], state[1] + moves[action][1])
     return new_state if 0 <= new_state[0] < grid_size and 0 <= new_state[0] < grid_size and 0 <= new_state[1] < grid_size and
     new state not in obstacles else state
+
+#training the model
+for _ in range(100) :
+    state = (0,0)
+    while state != goal:
+        action = random.choice(actions)
+        new_state = move(state, action)
+        reward = 10 if new_state == goal else (-5 if new_state in obstacles else -1)
+        q[state][actions.index(action)] += 0.5 * (reward + 0.9 * np.max(q[new_state]) -q[state][action.index(action)])
+        state = new_state
+
+#testing
+state, path = (0,0), [(0,0)]
+while state  != goal:
+    action = actions[np.argmax(q[state])]
+    state = move(state,action)
+    path.append(state)
+
+#draw the path
+grid = np.zeros((grid_size, grid_size))
+for oba in obstacles :
+    grid[obs] = -1
+    grid[goal] = 2
+
+plt.imshow(grid, cmap = "gray_r", origin ="upper")
+for (x,y) in path:
+    plt.text(y,x, "#", ha = "center", fontsize = 15, color ="red" if(x,y) != goal else "green"
+plt.xticks(range(grid_size))
+plt.yticks(range(grid_size))
+plt.grid(True)
+plt.show()
+
+             
